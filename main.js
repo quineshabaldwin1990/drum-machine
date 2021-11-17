c1e3a26eb5bcf0b5fc0970e2d4b24e49c908a148
quineshabaldwin13 <baldwinquinesha@gmailcom>
2021-11-17 10:09:18 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 // Setup 'tick' sound
 const tick = new Audio('sounds/tick.mp3');
+const tick = new Audio('sounds/tick.mp3');
+const tock = new Audio('sounds/tock.mp3');
+const kick = new Audio('sounds/kick-drum.mp3');
+const snare = new Audio('sounds/snare-drum.mp3');
+const hihat = new Audio('sounds/hi-hat.mp3');
+let count=0;
 
 // This function is called every 600ms
 function update() {
