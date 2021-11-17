68ead309f9cf0961ca505373e24bb243d84aa59d
quineshabaldwin13 <baldwinquinesha@gmailcom>
2021-11-17 10:12:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,34 @@ let count=0;
 
 // This function is called every 600ms
 function update() {
+    count++;
+    
+    const metronomeCount = (count % 4) + 1;
+    document.querySelector("#count").innerText=metronomeCount;
+
+    if (document.querySelector("#metronome").checked) {
+    
+        if (document.querySelector("#kick-drum").checked && Number(document.querySelector("#kick-drum-timing").value)===metronomeCount)
+            kick.play()
+        if (document.querySelector("#snare-drum").checked && Number(document.querySelector("#snare-drum-timing").value)===metronomeCount)
+            snare.play()
+        if (document.querySelector("#hi-hat").checked && Number(document.querySelector("#hi-hat-timing").value)===metronomeCount)
+            hihat.play()
+
+        if (metronomeCount===4) {
+            // Play the 'tock' sound
+            tock.play();
+
+        } else {
+            // Play the 'tick' sound
+            tick.play();
+        }
+
+
+    }
+
+}
+
 
     // Play the 'tick' sound
     tick.play();
